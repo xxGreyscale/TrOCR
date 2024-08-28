9c551e2713b611c4a7bbe4793da541a7cf6ba3f5
Salum Nassor <salim.nassor.sn@gmail.com>
2024-08-28 13:28:54 +0200

    improve: reduced the code to fetch configs from json file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 from data.datasets.handwritten.generate_dataset import GenerateDemoktatiLabourerDataset
 from data.datasets.printed.generate import GenerateSyntheticPrintedDataset
@@ -26,6 +27,8 @@ def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--generate_dataset', action=argparse.BooleanOptionalAction,
                         help='Generate dataset for training the model')
+    parser.add_argument('--train', action=argparse.BooleanOptionalAction,
+                        help='Train the model')
     parser.add_argument('--augment_data', action=argparse.BooleanOptionalAction,
                         help='Generate augmented dataset for training the model')
     parser.add_argument('--htr', action=argparse.BooleanOptionalAction,
@@ -34,28 +37,38 @@ def main():
                         help='Generate printed dataset for training printed text recognition model')
     parser.add_argument('--pretrained', action=argparse.BooleanOptionalAction,
                         help='Train a model with pretrained weights, specify the model')
-    parser.add_argument('--custom', action=argparse.BooleanOptionalAction, help='Model type')
     parser.add_argument('--files_path', nargs="+", help='Path to XML and images files')
     parser.add_argument('--save_dir', type=str, help='Directory to save the dataset')
     parser.add_argument('--lang', type=str, help='Language to use')
     parser.add_argument('--num_images', type=int, help='Number of images to generate')
-    parser.add_argument('--train', action=argparse.BooleanOptionalAction, help='Train the model')
-    parser.add_argument('--eval_every', type=int, help='Evaluate the model every n epochs')
+    parser.add_argument('--custom', action=argparse.BooleanOptionalAction, help='Model type')
     parser.add_argument('--dataset_paths', nargs="+", help='Path to the dataset')
-    parser.add_argument('--lr', type=float, help='Learning rate')
-    parser.add_argument('--epochs', type=int, help='Number of epochs')
-    parser.add_argument('--batch_size', type=int, help='Batch size')
-    parser.add_argument('--max_target_length', type=int, help='Maximum target length')
-    parser.add_argument('--decoder', type=str, help='Decoder type')
-    parser.add_argument('--tokenizer_type', type=str, help='Tokenizer type')
-    parser.add_argument('--encoder', type=str, help='Encoder type')
-    parser.add_argument('--model_version', type=str, help='Model version')
-    parser.add_argument('--processor', type=str, help='Processor path for the model')
-    parser.add_argument('--vision_encoder_decoder_model', type=str, help='Vision Encoder Decoder model')
     parser.add_argument('--with_half_data', action=argparse.BooleanOptionalAction,
                         help='Get the first half of the dataset')
+    parser.add_argument('--train_config_path', type=str, help='Path to the training configuration file')
+
+    def read_json_file(file_path):
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+        _config = Config(
+            learning_rate=data.get('lr'),
+            epochs=data.get('epochs'),
+            batch_size=data.get('batch_size'),
+            max_target_length=data.get('max_target_length'),
+            model_version=data.get('model_version'),
+            num_epochs=data.get('epochs'),  # Assuming num_epochs is same as epochs
+            decoder=data.get('decoder'),
+            tokenizer_type=data.get('tokenizer_type'),
+            encoder=data.get('encoder'),
+            image_processor_type=data.get('image_processor_type'),
+            eval_frequency=data.get('eval_frequency') # Evaluate the model every n epochs
+        )
+        return _config
+
+    # Usage
 
     args = parser.parse_args()
+    config = read_json_file(args.train_config_path)
     if args.generate_dataset:
         if args.htr:
             try:
@@ -75,22 +88,22 @@ def main():
     if args.train:
         if args.dataset_paths is None:
             raise ValueError("Please specify the dataset path")
-        if args.lr is None:
+        if config.learning_rate is None:
             raise ValueError("Please specify the learning rate")
-        if args.epochs is None:
+        if config.epochs is None:
             raise ValueError("Please specify the number of epochs")
-        if args.batch_size is None:
+        if config.batch_size is None:
             raise ValueError("Please specify the batch size")
-        if args.max_target_length is None:
+        if config.max_target_length is None:
             raise ValueError("Please specify the maximum target length")
-        if args.model_version is None:
+        if config.model_version is None:
             raise ValueError("Please specify the model version")
         # Check if we are using a pretrained model or a custom model
         if args.pretrained is not None:
             logger.info("Using pretrained model...")
-            transfer_learning(args)
+            transfer_learning(config, args)
         elif args.custom is not None:
-            train(args)
+            train(config, args)
         else:
             logger.error("Invalid model type. Please specify the model type")
 
@@ -103,22 +116,12 @@ def evaluate():
     pass
 
 
-def transfer_learning(args):
-    if args.processor is None:
+def transfer_learning(config: Config, args):
+    if config.processor is None:
         raise ValueError("Please specify the trocr model")
-    if args.vision_encoder_decoder_model is None:
+    if config.vision_encoder_decoder_model is None:
         raise ValueError("Please specify the Vision Encoder Decoder model")
     try:
-        config = Config(
-            learning_rate=args.lr,
-            epochs=args.epochs,
-            batch_size=args.batch_size,
-            max_target_length=args.max_target_length,
-            processor=args.processor,
-            vision_encoder_decoder_model=args.vision_encoder_decoder_model,
-            model_version=args.model_version,
-            num_epochs=args.epochs
-        )
         # get the dataset
         logger.info("Getting the dataset...")
         data = CustomLoader(args.dataset_paths)
@@ -126,39 +129,26 @@ def transfer_learning(args):
         # create the model
         logger.info("Creating the model...")
         model = TrOCR(config, args.save_dir)
-        model.build_model_with_pretrained(args.processor, args.vision_encoder_decoder_model)
+        model.build_model_with_pretrained(config.processor, config.vision_encoder_decoder_model)
         # prepare the dataset and loader
         if args.with_half_data:
             train_dataloader, eval_dataloader = model.set_data_loader(*model.prepare_dataset(data.get_half_dataframe()))
         else:
             train_dataloader, eval_dataloader = model.set_data_loader(*model.prepare_dataset(data.get_dataframe()))
         # train the model
-        model.train(train_dataloader, eval_dataloader, eval_every=args.eval_every)
+        model.train(train_dataloader, eval_dataloader, eval_every=config.eval_frequency)
     except Exception as e:
         logger.error(f"Error training model: {e}")
 
 
-def train(args):
-    if args.decoder is None:
+def train(config: Config, args):
+    if config.decoder is None:
         raise ValueError("Please specify the decoder type")
-    if args.tokenizer_type is None:
+    if config.tokenizer_type is None:
         raise ValueError("Please specify the tokenizer type")
-    if args.encoder is None:
+    if config.encoder is None:
         raise ValueError("Please specify the encoder type")
     try:
-        # create the configuration with wandb
-        # Initiate wandb
-        config = Config(
-            learning_rate=args.lr,
-            epochs=args.epochs,
-            batch_size=args.batch_size,
-            max_target_length=args.max_target_length,
-            decoder=args.decoder,
-            tokenizer_type=args.tokenizer_type,
-            encoder=args.encoder,
-            model_version=args.model_version,
-            num_epochs=args.epochs
-        )
         # get the dataset
         # data = CustomLoader("datasets/handwritten/augmented/image_labels_dataset.csv")
         logger.info("Getting the dataset...")
@@ -171,7 +161,7 @@ def train(args):
         # prepare the dataset and loader
         train_dataloader, eval_dataloader = model.set_data_loader(*model.prepare_dataset(data.get_dataframe()))
         # train the model
-        model.train(train_dataloader, eval_dataloader, eval_every=args.eval_every)
+        model.train(train_dataloader, eval_dataloader, eval_every=config.eval_frequency)
     except Exception as e:
         logger.error(f"Error training model: {e}")
 

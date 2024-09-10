90f07075b6a45ff66eb52ff65679ab84c23448b6
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-10 17:20:59 +0200

    feat: added where to save wandb logs

--- utils/config.py
+++ utils/config.py
@@ -13,7 +13,9 @@ class Config:
             processor=None,
             vision_encoder_decoder_model=None,
             encoder=None,
-            eval_frequency=1
+            eval_frequency=1,
+            log_dir=None  # logs for wandb
+
     ):
         self.learning_rate = learning_rate
         self.epochs = epochs
@@ -34,10 +36,10 @@ class Config:
         self.model_version = model_version
         self.num_epochs = num_epochs
         self.eval_frequency = eval_frequency
+        self.log_dir = log_dir
 
     def __str__(self):
         if hasattr(self, "processor"):
             return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, processor={self.processor}, vision_encoder_decoder_model={self.vision_encoder_decoder_model}), eval_frequency={self.eval_frequency})"
         if hasattr(self, "decoder"):
             return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, decoder={self.decoder}, tokenizer_type={self.tokenizer_type}, image processor={self.image_processor_type}, encoder={self.encoder}), eval_frequency={self.eval_frequency})"
-

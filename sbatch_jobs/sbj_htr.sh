6a942f0bcfcb36675adf7e3d933fcbbdfabd64d0
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-10 17:21:57 +0200

    refactor: add modules and activate conda

--- sbatch_jobs/sbj_htr.sh
+++ sbatch_jobs/sbj_htr.sh
@@ -4,10 +4,12 @@
 #SBATCH -C "fat"
 
 #SBATCH -t 01-16:00:00
-#SBATCH --output=logs/htr_training%j.text
+#SBATCH --output=/home/x_salom/logs/htr_training%j.text
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=salum.nassor.2008@student.uu.se
 
+module load Anaconda/2023.09-0-hpc1-bdist
+module load buildenv-gcccuda/11.8.0-gcc11.3.0
 cd /proj/berzelius-2024-46/users/x_salom/TrOCR
-conda activate ocr
+conda activate myocr
 python main.py --train --custom --dataset_paths datasets/handwritten/augmented_v2/image_labels_dataset.csv datasets/handwritten/normall/image_labels_dataset.csv --save_dir custom_models/trocr-handwritten/small --train_config_path configs/train/trOCR/handwritten/custom/htr.json

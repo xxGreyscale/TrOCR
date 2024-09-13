696039a1455146ac97c5cd2145eea1debc637f83
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-13 03:08:14 +0200

    feat: improve config domain for training configurations

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Config:
     def __init__(
             self,
@@ -43,3 +46,76 @@ class Config:
             return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, processor={self.processor}, vision_encoder_decoder_model={self.vision_encoder_decoder_model}), eval_frequency={self.eval_frequency})"
         if hasattr(self, "decoder"):
             return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, decoder={self.decoder}, tokenizer_type={self.tokenizer_type}, image processor={self.image_processor_type}, encoder={self.encoder}), eval_frequency={self.eval_frequency})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_dict(self):
+        if hasattr(self, "processor") and hasattr(self, "vision_encoder_decoder_model"):
+            return {
+                "learning_rate": self.learning_rate,
+                "epochs": self.epochs,
+                "batch_size": self.batch_size,
+                "max_target_length": self.max_target_length,
+                "model_version": self.model_version,
+                "num_epochs": self.num_epochs,
+                "processor": self.processor,
+                "vision_encoder_decoder_model": self.vision_encoder_decoder_model,
+                "eval_frequency": self.eval_frequency
+            }
+        if (hasattr(self, "decoder")
+                and hasattr(self, "tokenizer_type")
+                and hasattr(self, "image_processor_type")
+                and hasattr(self, "encoder")):
+            return {
+                "learning_rate": self.learning_rate,
+                "epochs": self.epochs,
+                "batch_size": self.batch_size,
+                "max_target_length": self.max_target_length,
+                "model_version": self.model_version,
+                "num_epochs": self.num_epochs,
+                "decoder": self.decoder,
+                "tokenizer_type": self.tokenizer_type,
+                "image_processor_type": self.image_processor_type,
+                "encoder": self.encoder,
+                "eval_frequency": self.eval_frequency
+            }
+
+    @staticmethod
+    def from_dict(data):
+        if "processor" in data:
+            return Config(
+                learning_rate=data["learning_rate"],
+                epochs=data["epochs"],
+                batch_size=data["batch_size"],
+                max_target_length=data["max_target_length"],
+                model_version=data["model_version"],
+                num_epochs=data["num_epochs"],
+                processor=data["processor"],
+                vision_encoder_decoder_model=data["vision_encoder_decoder_model"],
+                eval_frequency=data["eval_frequency"]
+            )
+        if "decoder" in data:
+            return Config(
+                learning_rate=data["learning_rate"],
+                epochs=data["epochs"],
+                batch_size=data["batch_size"],
+                max_target_length=data["max_target_length"],
+                model_version=data["model_version"],
+                num_epochs=data["num_epochs"],
+                decoder=data["decoder"],
+                tokenizer_type=data["tokenizer_type"],
+                image_processor_type=data["image_processor_type"],
+                encoder=data["encoder"],
+                eval_frequency=data["eval_frequency"]
+            )
+
+    @staticmethod
+    def from_json(file_path):
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+        return Config.from_dict(data)
+
+    def to_json(self, file_path):
+        with open(file_path, 'w') as json_file:
+            json.dump(self.to_dict(), json_file, indent=4)

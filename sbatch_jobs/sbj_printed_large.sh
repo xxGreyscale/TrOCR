ffbaf87f5484b6157f9e09b4ffc500a6ec90218a
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-17 01:58:09 +0200

    feat: added new configs

---
+++ sbatch_jobs/sbj_printed_large.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A Berzelius-2024-316
+#SBATCH --gpus 1
+#SBATCH -C "fat"
+
+#SBATCH -t 03-12:00:00
+#SBATCH --output=/home/x_salom/logs/printed_large%j.text
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=salum.nassor.2008@student.uu.se
+
+module load Anaconda/2023.09-0-hpc1-bdist
+module load buildenv-gcccuda/11.8.0-gcc11.3.0
+cd /proj/berzelius-2024-46/users/x_salom/TrOCR
+conda activate myocr
+python main.py --train --pretrained --dataset_paths datasets/printed/synthetic/110k/image_labels_dataset.csv datasets/printed/synthetic/130k/image_labels_dataset.csv datasets/printed/synthetic/beta/image_labels_dataset.csv datasets/printed/synthetic/beta_v2image_labels_dataset.csv --save_dir custom_models/trocr-printed/new/large --train_config_path configs/train/trOCR/printed/custom_printed.json

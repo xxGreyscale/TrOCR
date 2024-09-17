11eef42a150c699698ccf57c7243580132107e83
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-17 04:36:18 +0200

    fix: configs

--- utils/config.py
+++ utils/config.py
@@ -9,7 +9,6 @@ class Config:
             batch_size,
             max_target_length,
             model_version,
-            num_epochs,
             decoder=None,
             tokenizer_type=None,
             image_processor_type=None,
@@ -37,15 +36,14 @@ class Config:
             self.processor = processor
             self.vision_encoder_decoder_model = vision_encoder_decoder_model
         self.model_version = model_version
-        self.num_epochs = num_epochs
         self.eval_frequency = eval_frequency
         self.log_dir = log_dir
 
     def __str__(self):
         if hasattr(self, "processor"):
-            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, processor={self.processor}, vision_encoder_decoder_model={self.vision_encoder_decoder_model}), eval_frequency={self.eval_frequency})"
+            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, processor={self.processor}, vision_encoder_decoder_model={self.vision_encoder_decoder_model}), eval_frequency={self.eval_frequency})"
         if hasattr(self, "decoder"):
-            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, num_epochs={self.num_epochs}, decoder={self.decoder}, tokenizer_type={self.tokenizer_type}, image processor={self.image_processor_type}, encoder={self.encoder}), eval_frequency={self.eval_frequency})"
+            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, decoder={self.decoder}, tokenizer_type={self.tokenizer_type}, image processor={self.image_processor_type}, encoder={self.encoder}), eval_frequency={self.eval_frequency})"
 
     def __repr__(self):
         return self.__str__()
@@ -58,7 +56,6 @@ class Config:
                 "batch_size": self.batch_size,
                 "max_target_length": self.max_target_length,
                 "model_version": self.model_version,
-                "num_epochs": self.num_epochs,
                 "processor": self.processor,
                 "vision_encoder_decoder_model": self.vision_encoder_decoder_model,
                 "eval_frequency": self.eval_frequency
@@ -73,7 +70,6 @@ class Config:
                 "batch_size": self.batch_size,
                 "max_target_length": self.max_target_length,
                 "model_version": self.model_version,
-                "num_epochs": self.num_epochs,
                 "decoder": self.decoder,
                 "tokenizer_type": self.tokenizer_type,
                 "image_processor_type": self.image_processor_type,
@@ -90,7 +86,6 @@ class Config:
                 batch_size=data["batch_size"],
                 max_target_length=data["max_target_length"],
                 model_version=data["model_version"],
-                num_epochs=data["num_epochs"],
                 processor=data["processor"],
                 vision_encoder_decoder_model=data["vision_encoder_decoder_model"],
                 eval_frequency=data["eval_frequency"]
@@ -102,7 +97,6 @@ class Config:
                 batch_size=data["batch_size"],
                 max_target_length=data["max_target_length"],
                 model_version=data["model_version"],
-                num_epochs=data["num_epochs"],
                 decoder=data["decoder"],
                 tokenizer_type=data["tokenizer_type"],
                 image_processor_type=data["image_processor_type"],

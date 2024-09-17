bd04a951385ca1978520b3b63fe419f38f7ff74e
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-17 05:15:12 +0200

    fix: fix configs

--- sbatch_jobs/sbj_printed_pretrained_htr_medium.sh
+++ sbatch_jobs/sbj_printed_pretrained_htr_medium.sh
@@ -12,4 +12,4 @@ module load Anaconda/2023.09-0-hpc1-bdist
 module load buildenv-gcccuda/11.8.0-gcc11.3.0
 cd /proj/berzelius-2024-46/users/x_salom/TrOCR
 conda activate myocr
-python main.py --train --pretrained --dataset_paths datasets/printed/synthetic/110k/image_labels_dataset.csv datasets/printed/synthetic/130k/image_labels_dataset.csv --save_dir custom_models/trocr-printed/pretrained/medium --train_config_path configs/train/trOCR/printed/custom_pretrained_htr_small.json
+python main.py --train --pretrained --dataset_paths datasets/printed/synthetic/110k/image_labels_dataset.csv datasets/printed/synthetic/130k/image_labels_dataset.csv --save_dir custom_models/trocr-printed/pretrained/medium --config_path configs/train/trOCR/printed/custom_pretrained_htr_small.json

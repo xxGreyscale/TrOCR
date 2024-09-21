257670b83e200a6d25a4f697d62e80bd04912153
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-21 07:55:49 +0200

    fix: change directory in sbatch configs

--- sbatch_jobs/sbj_printed_pretrained_htr_medium.sh
+++ sbatch_jobs/sbj_printed_pretrained_htr_medium.sh
@@ -10,6 +10,6 @@
 
 module load Anaconda/2023.09-0-hpc1-bdist
 module load buildenv-gcccuda/11.8.0-gcc11.3.0
-cd /proj/berzelius-2024-46/users/x_salom/TrOCR
+cd ~/TrOCR
 conda activate myocr
 python main.py --train --pretrained --dataset_paths datasets/printed/synthetic/beta_v3/image_labels_dataset.csv --save_dir custom_models/trocr-printed/pretrained/medium --config_path configs/train/trOCR/printed/custom_pretrained_htr_small.json

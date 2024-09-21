3ec3c0efeb09e5beb17c526446d41e992f0880c0
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-22 01:13:21 +0200

    (refactor): conda activate ml-ocr

--- sbatch_jobs/sbj_htr.sh
+++ sbatch_jobs/sbj_htr.sh
@@ -11,5 +11,5 @@
 module load Anaconda/2023.09-0-hpc1-bdist
 module load buildenv-gcccuda/11.8.0-gcc11.3.0
 cd ~/TrOCR
-conda activate myocr
+conda activate ml-ocr
 python main.py --train --custom --dataset_paths datasets/handwritten/augmented_v2/image_labels_dataset.csv datasets/handwritten/normall/image_labels_dataset.csv --save_dir custom_models/trocr-handwritten/small --train_config_path configs/train/trOCR/handwritten/custom/htr.json

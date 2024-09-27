c86f96dc725dfe4e395589f17e08a5b9bd6c2a1f
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-27 21:39:42 +0200

    (config): added layer settings

--- utils/config.py
+++ utils/config.py
@@ -17,6 +17,9 @@ class Config:
             encoder=None,
             eval_frequency=1,
             test_dataset=None,
+            fine_tune_all_layers=False,
+            freeze_lower_layers=False,
+            freeze_upper_layers=False,
             wandb_project="TrOCR - Thesis Results",
             log_dir=None  # logs for wandb
     ):
@@ -37,6 +40,9 @@ class Config:
         if processor is not None and vision_encoder_decoder_model is not None:
             self.processor = processor
             self.vision_encoder_decoder_model = vision_encoder_decoder_model
+            self.freeze_upper_layers = freeze_upper_layers
+            self.freeze_lower_layers = freeze_lower_layers
+            self.fine_tune_all_layers = fine_tune_all_layers
         self.model_version = model_version
         self.eval_frequency = eval_frequency
         self.test_dataset = test_dataset
@@ -44,9 +50,34 @@ class Config:
 
     def __str__(self):
         if hasattr(self, "processor"):
-            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, processor={self.processor}, vision_encoder_decoder_model={self.vision_encoder_decoder_model}), eval_frequency={self.eval_frequency}, test_dataset={self.test_dataset}, log_dir={self.log_dir}, wandb_project={self.wandb_project})"
+            return (f"Config(learning_rate={self.learning_rate}, "
+                    f"epochs={self.epochs}, "
+                    f"batch_size={self.batch_size}, "
+                    f"max_target_length={self.max_target_length}, "
+                    f"model_version={self.model_version}, "
+                    f"processor={self.processor}, "
+                    f"vision_encoder_decoder_model={self.vision_encoder_decoder_model}), "
+                    f"eval_frequency={self.eval_frequency}, "
+                    f"test_dataset={self.test_dataset}, "
+                    f"log_dir={self.log_dir}, "
+                    f"freeze_upper_layers={self.freeze_upper_layers}, "
+                    f"freeze_lower_layers={self.freeze_lower_layers}, "
+                    f"fine_tune_all_layers={self.fine_tune_all_layers}, "
+                    f"wandb_project={self.wandb_project})")
         if hasattr(self, "decoder"):
-            return f"Config(learning_rate={self.learning_rate}, epochs={self.epochs}, batch_size={self.batch_size}, max_target_length={self.max_target_length}, model_version={self.model_version}, decoder={self.decoder}, tokenizer_type={self.tokenizer_type}, image processor={self.image_processor_type}, encoder={self.encoder}), eval_frequency={self.eval_frequency}, test_dataset={self.test_dataset}, log_dir={self.log_dir}, wandb_project={self.wandb_project})"
+            return (f"Config(learning_rate={self.learning_rate}, "
+                    f"epochs={self.epochs}, "
+                    f"batch_size={self.batch_size}, "
+                    f"max_target_length={self.max_target_length}, "
+                    f"model_version={self.model_version}, "
+                    f"decoder={self.decoder}, "
+                    f"tokenizer_type={self.tokenizer_type}, "
+                    f"image processor={self.image_processor_type}, "
+                    f"encoder={self.encoder}), "
+                    f"eval_frequency={self.eval_frequency}, "
+                    f"test_dataset={self.test_dataset}, "
+                    f"log_dir={self.log_dir}, "
+                    f"wandb_project={self.wandb_project})")
 
     def __repr__(self):
         return self.__str__()
@@ -61,6 +92,9 @@ class Config:
                 "model_version": self.model_version,
                 "processor": self.processor,
                 "vision_encoder_decoder_model": self.vision_encoder_decoder_model,
+                "freeze_upper_layers": self.freeze_upper_layers,
+                "freeze_lower_layers": self.freeze_lower_layers,
+                "fine_tune_all_layers": self.fine_tune_all_layers,
                 "test_dataset": self.test_dataset,
                 "wandb_project": self.wandb_project,
                 "eval_frequency": self.eval_frequency
@@ -95,7 +129,10 @@ class Config:
                 model_version=data["model_version"],
                 processor=data["processor"],
                 vision_encoder_decoder_model=data["vision_encoder_decoder_model"],
-                test_dataset= data["test_dataset"] if len(data["test_dataset"]) > 0 else None,
+                test_dataset=data["test_dataset"] if len(data["test_dataset"]) > 0 else None,
+                freeze_upper_layers=data["freeze_upper_layers"] if "freeze_upper_layers" in data else False,
+                freeze_lower_layers=data["freeze_lower_layers"] if "freeze_lower_layers" in data else False,
+                fine_tune_all_layers=data["fine_tune_all_layers"] if "fine_tune_all_layers" in data else False,
                 eval_frequency=data["eval_frequency"],
                 wandb_project=data["wandb_project"]
             )
@@ -110,7 +147,7 @@ class Config:
                 tokenizer_type=data["tokenizer_type"],
                 image_processor_type=data["image_processor_type"],
                 encoder=data["encoder"],
-                test_dataset= data["test_dataset"] if len(data["test_dataset"]) > 0 else None,
+                test_dataset=data["test_dataset"] if len(data["test_dataset"]) > 0 else None,
                 eval_frequency=data["eval_frequency"],
                 wandb_project=data["wandb_project"]
             )

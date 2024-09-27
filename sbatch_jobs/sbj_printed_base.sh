44c8f4ad66c17861c2a28c49b8efc25d47962344
Salum Nassor <salim.nassor.sn@gmail.com>
2024-09-27 23:21:21 +0200

    feat: new sbatch job created

--- sbatch_jobs/sbj_printed_base.sh
+++ sbatch_jobs/sbj_printed_base.sh
@@ -12,4 +12,4 @@ module load Anaconda/2023.09-0-hpc1-bdist
 module load buildenv-gcccuda/11.8.0-gcc11.3.0
 cd ~/TrOCR
 conda activate ml-ocr
-python main.py --train --custom --dataset_paths datasets/printed/synthetic/beta_v1/image_labels_dataset.csv datasets/printed/synthetic/beta_v2/image_labels_dataset.csv datasets/printed/synthetic/beta_v3/image_labels_dataset.csv --save_dir custom_models/trocr-printed/scratch/large --config_path configs/train/trOCR/printed/custom_printed.json
+python main.py --train --custom --dataset_paths datasets/printed/synthetic/beta_v2/image_labels_dataset.csv datasets/printed/synthetic/beta_v3/image_labels_dataset.csv --save_dir custom_models/trocr-printed/scratch/base --config_path configs/train/trOCR/printed/custom_printed.json

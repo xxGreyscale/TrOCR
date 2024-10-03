f876b5c052428d8de23205c9029563edbadc443a
Salum Nassor <salim.nassor.sn@gmail.com>
2024-10-03 02:28:20 +0200

    chore: add more dataset for training

--- sbatch_jobs/sbj_printed_pretrained_htr_FF.sh
+++ sbatch_jobs/sbj_printed_pretrained_htr_FF.sh
@@ -12,4 +12,4 @@ module load Anaconda/2023.09-0-hpc1-bdist
 module load buildenv-gcccuda/11.8.0-gcc11.3.0
 cd ~/TrOCR
 conda activate ml-ocr
-python main.py --train --pretrained --dataset_paths datasets/printed/synthetic/beta_v2/image_labels_dataset.csv datasets/printed/synthetic/beta_v3/image_labels_dataset.csv --save_dir custom_models/trocr-printed/pretrained/fft --config_path configs/train/trOCR/printed/custom_pretrained_htr_fine_all_tune_layers.json
+python main.py --train --pretrained --dataset_paths datasets/printed/Histrorical_News_Paper/image_labels_dataset.csv datasets/printed/synthetic/beta_v1/image_labels_dataset.csv datasets/printed/synthetic/beta_v2/image_labels_dataset.csv datasets/printed/synthetic/beta_v3/image_labels_dataset.csv --save_dir custom_models/trocr-printed/pretrained/fft --config_path configs/train/trOCR/printed/custom_pretrained_htr_fine_all_tune_layers.json
